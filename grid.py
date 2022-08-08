bba0778f5663f08e4f5efb159416cc2847dfce83
Praise <somtoeneh@gmail.com>
2022-08-07 21:15:34 -0500

    working on grid

--- grid.py
+++ grid.py
@@ -1,22 +1,37 @@
 import pygame
 import numpy as np
 
-black = (0, 0, 0)
-white = (255, 255, 255)
-
-red = (255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 WIDTH = 20
 HEIGHT = 20
 MARGIN = 5
 NUM_COL = 15
 NUM_ROW = 10
-grid = np.zeros((NUM_ROW, NUM_COL))
-grid[1][5] = 1
+GRID = np.zeros((NUM_ROW, NUM_COL))
+GRID[0][0] = 1
 pygame.init()
 window_size = [400, 400]
-scr = pygame.display.set_mode(window_size)
+WIN = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Grid")
 clock = pygame.time.Clock()
+
+
+def update_grid():
+    WIN.fill(BLACK)
+    for row in range(NUM_ROW):
+        for column in range(NUM_COL):
+            color = WHITE
+            if GRID[row][column] == 1:
+                color = RED
+            pygame.draw.rect(WIN,
+                             color,
+                             [(MARGIN + WIDTH) * column + MARGIN,
+                              (MARGIN + HEIGHT) * row + MARGIN,
+                              WIDTH,
+                              HEIGHT])
+    pygame.display.flip()
 
 
 def main():
@@ -32,20 +47,9 @@ def main():
                     pos = pygame.mouse.get_pos()
                     column = pos[0] // (WIDTH + MARGIN)
                     row = pos[1] // (HEIGHT + MARGIN)
-                    grid[row][column] = 1
+                    GRID[row][column] = 1
                     print("Click ", pos, "Grid coordinates: ", row, column)
-        scr.fill(black)
-        for row in range(NUM_ROW):
-            for column in range(NUM_COL):
-                color = white
-                if grid[row][column] == 1:
-                    color = red
-                pygame.draw.rect(scr,
-                                 color,
-                                 [(MARGIN + WIDTH) * column + MARGIN,
-                                  (MARGIN + HEIGHT) * row + MARGIN,
-                                  WIDTH,
-                                  HEIGHT])
+        update_grid()
         pygame.display.flip()
     pygame.quit()
 
